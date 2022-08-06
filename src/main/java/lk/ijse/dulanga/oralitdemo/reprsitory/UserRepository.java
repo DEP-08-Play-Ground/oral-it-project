f50abc6d7b96d5c6ca5417c8e6bb1fc490c7f3c4
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-08-06 11:00:59 +0530

    Create the existsUserByEmail method

--- src/main/java/lk/ijse/dulanga/oralitdemo/reprsitory/UserRepository.java
+++ src/main/java/lk/ijse/dulanga/oralitdemo/reprsitory/UserRepository.java
@@ -4,4 +4,6 @@ import lk.ijse.dulanga.oralitdemo.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User,String> {
+    boolean existsUserByEmail(String email);
+
 }
