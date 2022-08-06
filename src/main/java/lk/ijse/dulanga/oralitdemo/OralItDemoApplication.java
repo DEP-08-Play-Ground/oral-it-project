7ccccd1ebed55d517adc3f9eb1bb5ff9e47f40b3
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-08-06 09:48:58 +0530

    Add the relevant properties and Initialize

---
+++ src/main/java/lk/ijse/dulanga/oralitdemo/OralItDemoApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dulanga.oralitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OralItDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OralItDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/dulanga/oralitdemo/OralItDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dulanga.oralitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OralItDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
