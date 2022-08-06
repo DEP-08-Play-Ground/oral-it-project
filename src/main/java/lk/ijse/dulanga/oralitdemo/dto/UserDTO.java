29e4317da1a70bffa5f41a78d8946b3081734d89
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-08-06 10:20:05 +0530

    Create the UserDTO

--- src/main/java/lk/ijse/dulanga/oralitdemo/dto/UserDTO.java
+++ src/main/java/lk/ijse/dulanga/oralitdemo/dto/UserDTO.java
@@ -1,5 +1,38 @@
 package lk.ijse.dulanga.oralitdemo.dto;
 
 public class UserDTO {
+    private String email;
+    private String mobileNO;
 
+    public UserDTO() {
+    }
+
+    public UserDTO(String email, String mobileNO) {
+        this.email = email;
+        this.mobileNO = mobileNO;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNO() {
+        return mobileNO;
+    }
+
+    public void setMobileNO(String mobileNO) {
+        this.mobileNO = mobileNO;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "email='" + email + '\'' +
+                ", mobileNO='" + mobileNO + '\'' +
+                '}';
+    }
 }
