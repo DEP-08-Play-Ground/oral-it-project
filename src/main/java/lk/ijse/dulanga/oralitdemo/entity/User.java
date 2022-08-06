298e8fdf541d9a6d805bfd09ebe8c5db89226e61
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-08-06 10:32:04 +0530

    Create the User Entity

--- src/main/java/lk/ijse/dulanga/oralitdemo/entity/User.java
+++ src/main/java/lk/ijse/dulanga/oralitdemo/entity/User.java
@@ -1,4 +1,20 @@
 package lk.ijse.dulanga.oralitdemo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements SuperEntity {
+    @Id
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String mobileNO;
 }
